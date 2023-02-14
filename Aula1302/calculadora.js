a10d64489ec441eedb8c86f8f750d0b5ab005b18
rafacalera <rafa_calera@hotmail.com>
2023-02-14 20:42:52 -0300

    Aula 1402 - Trabalhando com módulos e metodos

---
+++ Aula1302/calculadora.js
@@ -0,0 +1,5 @@
+module.exports = {
+    media(t1, t2, n1, n2){
+        return ((t1* 0.2) + (t2 * 0.2) + (n1 * 0.3) + (n2 * 0.3))
+    }
+}
--- Aula1302/index.js
+++ Aula1302/index.js
@@ -1,9 +1,18 @@
 const chalk = require("chalk")
+const calculadora = require("./calculadora")
 const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 })
-readline.question("Qual sua linguagem de programação favorita: ", (linguagem) =>{
-    console.log(chalk.red("Sua linguagem favorita é: ")+linguagem)
-    readline.close()
-}) 
+
+var nota = calculadora.media(1, 5, 5 , 6)
+
+if(nota >= 6){
+    console.log(chalk.green("Você está aprovado: ") + chalk.black.bgGreen(nota))
+}
+else if (nota >= 5){
+    console.log(chalk.yellow("Você está de recuperação: ") + chalk.black.bgYellow(nota))
+}
+else{
+    console.log(chalk.red("Você está reprovado: ") + chalk.black.bgRed(nota))
+}
